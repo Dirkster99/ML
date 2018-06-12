bec024b81adf7ba8a7272b651b8e42d220a3b7a6
Dirkster99 <Dirk.Bahle@gmx.de>
2018-06-12 19:05:45 +0200

    Initial Commit

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # SentimentAnalysis
 Sample Project for Sentiment Detection using ML.Net
+
+This project contains a "Hello World" machine learning .Net sample project from here:
+
+https://docs.microsoft.com/en-us/dotnet/machine-learning/tutorials/sentiment-analysis
+
+The project show cases sentiment detection through classification of text.
---
+++ source/SentimentAnalysis/SentimentData.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.ML.Runtime.Api;
+
+namespace SentimentAnalysis
+{
+    /// <summary>
+    /// This is the input dataset class and has a float (Sentiment) that has
+    /// a value for sentiment of either positive or negative, and a string for
+    /// the comment.
+    /// 
+    /// Both fields have Column attributes attached to them.
+    /// This attribute describes the order of each field in the data file,
+    /// and which is the Label field.
+    /// </summary>
+    public class SentimentData
+    {
+        [Column(ordinal: "0", name: "Label")]
+        public float Sentiment;
+
+        [Column(ordinal: "1")]
+        public string SentimentText;
+    }
+
+    /// <summary>
+    /// This is the class used for prediction after the model has been trained.
+    /// It has a single boolean  <see cref="Sentiment"/> and a PredictedLabel
+    /// ColumnName attribute.
+    /// 
+    /// The Label is used to create and train the model, and it's also used with a
+    /// second dataset to evaluate the model. The PredictedLabel is used during
+    /// prediction and evaluation. For evaluation, an input with training data,
+    /// the predicted values, and the model are used.
+    /// </summary>
+    public class SentimentPrediction
+    {
+        [ColumnName("PredictedLabel")]
+        public bool Sentiment;
+    }
+}
